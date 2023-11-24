b741eac5b8457bb34c057f9180a27eab8cba5fd3
garethmcm <g.moffitt27@gmail.com>
2023-11-24 13:53:31 +0000

    re commit

---
+++ soundo/src/Advanced components/AdvancedLanding.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function AdvancedLanding() {
+  return (
+    <section>
+      <p>This is how sound <i>really</i>works.</p>
+    </section>
+  );
+}
+
+export default AdvancedLanding;

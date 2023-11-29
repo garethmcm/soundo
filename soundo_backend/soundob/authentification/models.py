bae7a76b858a0f134dc2641f9e7ce300150c5013
Gareth <gareth@Gareths-MacBook-Pro.local>
2023-11-29 20:44:34 +0000

    changed backend source

--- soundo_backend/soundob/authentification/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class userdb(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
--- soundo_backend/soundob/authentification/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import userdb
-
-class UserSerializer(serializers.Serializer):
-    class Meta:
-        model = userdb
-        fields = ('username', 'password', 'first_name', 'last_name')
